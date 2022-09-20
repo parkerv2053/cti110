b59b4d704ed929a71a74a4f93a9b4308dd7e171b
parkerv2053 <113809447+parkerv2053@users.noreply.github.com>
2022-09-20 08:22:49 -0400

    Add files via upload
    Second upload

---
+++ P1HW1_ParkerVictor.py
@@ -0,0 +1,17 @@
+user_num = int(input())
+
+print('Enter integer:')
+
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num*user_num)
+print('And', user_num, 'cubed is', user_num*user_num*user_num, '!!')
+
+
+user_num2 = int(input())
+
+print('Enter another integer:')
+
+print(user_num, '+', user_num2,'is', user_num+user_num2)
+print(user_num, '*', user_num2,'is', user_num*user_num2)
+
+    
