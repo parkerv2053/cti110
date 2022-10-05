c73bc2975d6cb7a5f029b18d4b8694c4bb147506
parkerv2053 <113809447+parkerv2053@users.noreply.github.com>
2022-10-04 23:49:41 -0400

    Add files via upload
    this number 6 I belive

---
+++ P2LAB_Stats_ParkerVictor.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+multiplied = float(num1 * num2 * num3 * num4)
+average = float((num1 + num2 + num3 + num4) / 4)
+
+print(f'{multiplied:.0f} {average:.0f}')
+print(f'{multiplied:.3f} {average:.3f}')
